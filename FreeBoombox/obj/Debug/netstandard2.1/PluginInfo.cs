5779d9aa4075ee17f864e9a685554f0448fc2e23
Luke Davidson <luke@lukedavidson.org>
2023-12-15 22:33:53 -0800

    Initial commit

---
+++ FreeBoombox/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace FreeBoombox
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "FreeBoombox";
+        public const string PLUGIN_NAME = "FreeBoombox";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
